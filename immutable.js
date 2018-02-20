24f691144719a5c48b3f911cfc9d529d35b4fc61
Tyson Williams <tyson.j.williams@gmail.com>
2018-02-20 14:12:42 -0700

    Complete challenges in Immutable.js and Redux paired programming challenges

--- immutable.js
+++ immutable.js
@@ -15,6 +15,16 @@
   Be sure to console.log() both references to ensure that they are pointing to different objects.
 */
 
+const person = {
+  name: "Tyson",
+  age: "31",
+  hometown: "Syracuse"
+}
+
+const newPerson = Object.assign({}, person);
+newPerson.name = "Ryan";
+newPerson.age = 27;
+
 console.log(person);
 console.log(newPerson);
 
@@ -23,6 +33,10 @@ console.log(newPerson);
   Create a separate array of `superVillains`
   Concatenate the two of them together using `.concat()`
 */
+
+const superHeroes = ["Batman", "Superman", "Spiderman", "Ironman"];
+const superVillains = ["Joker", "Lex Luther", "Venom", "Ironman Villain"];
+const combinedSupers = superHeroes.concat(superVillains);
 
 console.log(superHeroes);
 console.log(superVillains);
@@ -35,6 +49,14 @@ console.log(combinedSupers);
   Create a new object `studentAverage` using Object.assign() from the given original object.
   The new object should filter out any scores that are < 34 Array.filter should be used here.
 */
+
+const studentScores = {
+  name: "Tyson",
+  age: 31,
+  scores: [1, 25, 36, 47, 50],
+}
+
+const studentAverage = Object.assign({}, studentScores.scores.filter(score => score < 34));
 
 console.log(studentScores);
 console.log(studentAverage);
